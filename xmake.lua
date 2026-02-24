fc6bb9ee27147c00e94dc5eb547ce9fa82ba41d0
NoobOier2011 <guozisheng2022@126.com>
2026-02-24 23:17:49 +0800

    build: rewrite xmake.lua for the window function

--- xmake.lua
+++ xmake.lua
@@ -11,8 +11,25 @@ target("NeoStar")
     set_kind("binary")
     add_files("src/*.cpp")
 
+    -- 直接链接本地 GLFW 库
+    if is_plat("linux") then
+        if os.isfile("lib/libglfw.so") then
+            add_links("glfw")
+        elseif os.isfile("lib/libglfw3.a") then
+            add_links("glfw3")
+        end
+        
+    elseif is_plat("windows") then
+        if os.isfile("lib/glfw3.dll") then
+            add_links("glfw3dll")
+        else
+            add_links("glfw3")
+        end
+        add_syslinks("opengl32", "gdi32", "user32")
+    end
+
     -- lib and include
-    add_includedirs("include")
+    add_includedirs("include", "core")
     add_linkdirs("lib")
 
     -- MODE
@@ -20,5 +37,9 @@ target("NeoStar")
         add_defines("DEBUG")
     end
 
-    -- System Links (For windows)
-    add_syslinks("opengl32", "gdi32", "user32")
+    add_packages("glfw")
+
+    -- Compiler
+    set_toolchains("gcc")
+    set_toolset("cc", "gcc")
+    set_toolset("cxx", "g++")
